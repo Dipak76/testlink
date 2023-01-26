c9fba46ba6c5ea667d36309b6d4b08012cb273f0
Deepak Yadav <107499257+Dipak76@users.noreply.github.com>
2023-01-27 00:01:19 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,11 @@
+
+
+
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        sum=arr[i]+arr[j];
+        if(sum===value){
+            console.log(arr[i]+" "+arr[]);
+        }
+    }
+}
